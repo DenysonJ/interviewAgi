fd75083ecf5c641126db6be9020e486bcee3d206
Denyson Grellert <denysonjurgen@gmail.com>
2023-05-08 11:49:47 -0300

    Adicionar baseUrl e viewport para Desktop

--- cypress.config.js
+++ cypress.config.js
@@ -4,8 +4,11 @@ module.exports = defineConfig({
   video: false,
   numTestsKeptInMemory: 0,
   watchForFileChanges: false,
+  viewportWidth: 1060,
+  viewportHeight: 760,
   retries: 1,
   e2e: {
+    baseUrl: 'https://blogdoagi.com.br',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
