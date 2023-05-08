ea902030a5d877ae448f278545f9815d5d104a2e
Denyson Grellert <denysonjurgen@gmail.com>
2023-05-08 09:34:41 -0300

    Adicionar configurações do Cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  video: false,
+  numTestsKeptInMemory: 0,
+  watchForFileChanges: false,
+  retries: 1,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
