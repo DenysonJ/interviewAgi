ba70b2d05010d8f5c85b047909263fcafe39192e
DenysonJ <denysonjurgen@gmail.com>
2023-05-08 11:53:48 -0300

    Mapear elementos do blog

---
+++ cypress/support/pages/blogElements.js
@@ -0,0 +1,14 @@
+export const ELEMENTS = {
+  buttonOpenSearch: '#search-open',
+  inputSearch: '.desktop-search > .search-form > label > .search-field',
+  buttonSearch: '.desktop-search > .search-form > .search-submit',
+  titleSearch: '.archive-title',
+  titleNoResults: '.entry-title',
+  spanNoResults: '.entry-content > p'
+}
+
+export const TEXT = {
+  noResults: 'Não encontramos nada para estes termos de busca. Tente novamente com palavras-chave diferentes.',
+  titleNoResults: 'Nenhum resultado',
+  titleSearch: 'Resultados da busca por:'
+}
